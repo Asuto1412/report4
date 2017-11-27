c822c3691216d6e49dfd7c51aa5c62e856b77562
Sho Yoshida <e175758@ie.u-ryukyu.ac.jp>
2017-11-28 08:49:14 +0900

    second

--- src/report4.java
+++ src/report4.java
@@ -2,10 +2,10 @@ public class report4 {
     public static void main(String[] args) {
         try {
             String str = null;
-            System.out.println(str.length());
+            System.out.println(str.length()) ;
         }
         catch(NullPointerException empty) {
-            System.out.println("NullPointExceptinが発生!");
+            System.out.println("NullPointerException発生！");
             System.out.println(empty.getMessage());
         }
     }
