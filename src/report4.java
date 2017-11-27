c08ab2cf1a18c6c82ab6377806204eb7a0d8c2bd
Sho Yoshida <e175758@ie.u-ryukyu.ac.jp>
2017-11-28 08:50:34 +0900

    second

--- src/report4.java
+++ src/report4.java
@@ -1,12 +1,6 @@
 public class report4 {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length()) ;
-        }
-        catch(NullPointerException empty) {
-            System.out.println("NullPointerException発生！");
-            System.out.println(empty.getMessage());
-        }
+        String str = "3．14";
+        double value = Double.parseDouble(str);
     }
 }
